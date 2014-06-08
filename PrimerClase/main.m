a369b4c14cab99449fa7cc0054bc2f25a008839e
Carlos Azofeifa <CarlosAzofeifa@CazofeifaOSX.local>
2014-06-08 10:54:26 -0600

    Initial Commit

---
+++ PrimerClase/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  PrimerClase
+//
+//  Created by Carlos Azofeifa on 6/8/14.
+//  Copyright (c) 2014 cazofeifacr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
